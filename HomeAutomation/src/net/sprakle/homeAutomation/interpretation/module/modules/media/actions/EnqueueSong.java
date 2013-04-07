fda53abc01d00e73235c368ce1ac2a20b91cbd31
Sprakle <ben.cracknell.96@gmail.com>
2013-04-07 15:23:13 -0700

    Added enqueueTrack (Title + Artist) to Media Actions

--- HomeAutomation/src/net/sprakle/homeAutomation/interpretation/module/modules/media/actions/EnqueueSong.java
+++ HomeAutomation/src/net/sprakle/homeAutomation/interpretation/module/modules/media/actions/EnqueueSong.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 
 import net.sprakle.homeAutomation.interpretation.Phrase;
 import net.sprakle.homeAutomation.interpretation.module.modules.media.MediaAction;
-import net.sprakle.homeAutomation.interpretation.tagger.ParseHelpers;
 import net.sprakle.homeAutomation.interpretation.tagger.PhraseOutline;
 import net.sprakle.homeAutomation.interpretation.tagger.Tagger;
 import net.sprakle.homeAutomation.interpretation.tagger.tags.Tag;
 import net.sprakle.homeAutomation.interpretation.tagger.tags.TagType;
 import net.sprakle.homeAutomation.utilities.externalSoftware.software.media.MediaCentre;
+import net.sprakle.homeAutomation.utilities.logger.LogSource;
 import net.sprakle.homeAutomation.utilities.logger.Logger;
 
 public class EnqueueSong extends MediaAction {
-
-	// TODO: support artist names
 
 	public EnqueueSong(Logger logger, MediaCentre mc, Tagger tagger) {
 		super(logger, mc, tagger);
@@ -25,22 +23,50 @@ public class EnqueueSong extends MediaAction {
 
 		ArrayList<PhraseOutline> outlines = new ArrayList<PhraseOutline>();
 
-		PhraseOutline poA = new PhraseOutline(logger, tagger, getName());
+		PhraseOutline poA = new PhraseOutline(logger, tagger, getName() + " (title only)");
 		poA.addTag(new Tag(TagType.PLAYBACK, "play", null, -1));
 		poA.addTag(new Tag(TagType.UNKOWN_TEXT, null, null, -1));
 		poA.addTag(new Tag(TagType.TIME_CHANGE, "next", null, -1));
 
+		PhraseOutline poB = new PhraseOutline(logger, tagger, getName() + " (title + artist)");
+		poB.addTag(new Tag(TagType.PLAYBACK, "play", null, -1));
+		poB.addTag(new Tag(TagType.UNKOWN_TEXT, null, null, -1));
+		poB.addTag(new Tag(TagType.POSSESSION, "owned", null, -1));
+		poB.addTag(new Tag(TagType.UNKOWN_TEXT, null, null, -1));
+		poB.addTag(new Tag(TagType.TIME_CHANGE, "next", null, -1));
+
 		outlines.add(poA);
+		outlines.add(poB);
 
 		return outlines;
 	}
 
 	@Override
 	public void doExecute(Phrase phrase) {
-		Tag titleTag = ParseHelpers.getTagOfType(logger, tagger, TagType.UNKOWN_TEXT, phrase);
-		String titleString = titleTag.getValue();
+		// get the {UNKOWN_TEXT} tag value after the {POSSESION/BY} tag
+		ArrayList<Tag> tags = phrase.getTags();
 
-		mc.enqueueTrack(titleString, null);
+		int ownedTagIndex = -1;
+		for (Tag t : tags) {
+
+			if (t.getType() == TagType.POSSESSION && t.getValue().equals("owned"))
+				ownedTagIndex = tags.indexOf(t);
+		}
+
+		Tag titleTag = tags.get(ownedTagIndex - 1);
+		Tag artistTag = tags.get(ownedTagIndex + 1);
+
+		String title = null;
+		String artist = null;
+
+		if (titleTag != null & artistTag != null) {
+			title = titleTag.getValue();
+			artist = artistTag.getValue();
+		} else {
+			logger.log("Unable to determine track from phrase", LogSource.ERROR, LogSource.EXTERNAL_SOFTWARE, 1);
+		}
+
+		mc.enqueueTrack(title, artist);
 	}
 	@Override
 	public String getName() {
